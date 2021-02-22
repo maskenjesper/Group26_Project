c05737f2a9923a594ac694350a057d9ac7ef769c
Jakob Olsson <jakobolsson973@gmail.com>
2021-02-22 20:17:51 +0100

    fresh start

--- main.c
+++ main.c
@@ -12,8 +12,6 @@
 #include "lib.h"
 
 int timeoutcount = 0;
-Shape *S1;
-int locked;
 
 void user_isr () {
 
@@ -24,16 +22,10 @@ void user_isr () {
 
 	/********** TMR2 Interrupt **********/
 	if ((IFS(0) >> 8) & 0x1) {		// Tick
-		if (getbtns() & 0x1)
-			gameplan_moveShape(S1, RIGHT);
-		if (getbtns() >> 1 & 0x1)
-			gameplan_moveShape(S1, LEFT);
-		draw_frame();
+
 	}
 	if ((IFS(0) >> 8) & 0x1 && timeoutcount++ == 1) {	// Move testshape
 		timeoutcount = 0;
-		if (gameplan_moveShape(S1, DOWN))
-			locked = 0;
 
 	}
 
@@ -46,28 +38,7 @@ int main () {
 
 	init();
 
-	enum shape Shapes[100];
-	int i;
-	for (i = 0; i < 100; i++)
-		Shapes[i] = LLEFT;
-
-	Shapes[1] = BOX;
-	Shapes[2] = STICK;
-	Shapes[3] = T;
-	Shapes[4] = LRIGHT;
-	Shapes[5] = ZLEFT;
-	Shapes[6] = ZRIGHT;
-
-	while (1)
-		for (i = 0; i < 100; i++ ) {
-			locked = 1;
-
-			Shape temp = new_shape_la(Shapes[i], 0, 3, 1, 0);
-			S1 = &temp;
-			
-			while (locked);
-		}
-
+	
 
 	return 0;
 }
